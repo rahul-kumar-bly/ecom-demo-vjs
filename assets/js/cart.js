9e8904005f460ad645c727727b6212facf2165d1
Rahul Kumar <rahul.k.mak@gmail.com>
2024-12-22 13:57:18 +0530

    cart issues fixed and code refactored

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,70 +1,120 @@
-const fetchCartData = async (data) => {
-    try {
-    const response = await fetch(data);
-    if (!response.ok){
-        console.log(`response is not ok`, response.status);
+let generateCartItem = (product) => {
+    const {id, name, price, img} = product;
+    const productDiv = document.createElement('div');
+    productDiv.classList.add('product');
+    productDiv.id = `product-${id}`;
+
+    productDiv.innerHTML = `
+        <a href="./product.html?id=${id}">
+            <div class="product-image" id="productImage">
+                <img src="${img}" alt="${name}">
+            </div>                
+        </a>
+        <div class="product-details">
+            <div class="product-name" id="productName">
+                <h3>${name}</h3>
+            </div>
+            <div class="product-price" id="productPrice">
+                <p>${price}</p>
+            </div>
+            <div class="cart-operations" id="cartOperations-${id}">
+                <div class="move-to-wishlist"></div>
+                <div class="remove-from-cart"></div>
+            </div>
+        </div>
+    `;
+
+    const moveToWishlistButton = document.createElement('button');
+    moveToWishlistButton.classList.add('move-to-wishlist-button');
+    moveToWishlistButton.innerText = 'Move To Wishlist';
+
+    const isInWishlist = localWishlist.some(item => item.id === id);
+    if (isInWishlist) {
+        moveToWishlistButton.innerText = 'Item in Wishlist';
+        moveToWishlistButton.disabled = true;
     }
-    return await response.json();
-    } catch (error){
-        console.log(`error loading data ${error}`);
-    }
+
+    moveToWishlistButton.addEventListener('click', () => {
+        moveToWishlist(id);
+    });
+
+    const moveToWishlistDiv = productDiv.querySelector('.move-to-wishlist');
+    moveToWishlistDiv.appendChild(moveToWishlistButton);
+
+    const removeFromCartButton = document.createElement('button');
+    removeFromCartButton.classList.add('remove-from-cart');
+    removeFromCartButton.innerText = 'Remove from cart';
+
+    removeFromCartButton.addEventListener('click', () => {
+        removeFromCart(id);
+    })
+
+    const removeFromCartDiv = productDiv.querySelector('.remove-from-cart');
+    removeFromCartDiv.appendChild(removeFromCartButton);
+    return productDiv;
 }
 
 let generateCartItems = async () => {
-    let cartItems = calculateCartItems();
-    console.log('this function hasbeen called!')
-    const data = await fetchCartData('../../data/data.json');
-    if (cartItems.length > 0){
-        const shoppingCart = document.getElementById('products');
-        shoppingCart.innerHTML = cartItems.map((item) => {
-            const {id} = item;
-            let search = data.gameData.find((dataItem) => dataItem.id === id) || [];
-            return `
-                <div class="product" id="product">
-                    <a href="./product.html?id=${search.id}">
-                    <div class="product-image" id="productImage">
-                        <img src="${search.img}" alt="${search.name}">
-                    </div>                
-                    </a>
-                    <div class="product-details">
-                    <div class="product-name" id="productName">
-                        <h3>${search.name}</h3>
-                    </div>
-                    <div class="product-price" id="productPrice">
-                        <p>${search.price}</p>
-                    </div>
-                    <div class="cart-operations">
-                    <div class="move-to-wishlist" id="moveToWishlit">
-                        <button>Move to wishlist</button>
-                    </div>                    
-                    <div class="remove-from-cart" id="removeFromCart">
-                        <button onclick="removeFromCart(${search.id})"><i class="bi bi-trash"></i></button>
-                    </div>
-                    </div>
-                    </div>
-                </div>
-            `
-        }).join("")
+    const data = await loadData('../../data/data.json');
+    const shoppingCart = document.getElementById('products');
+    if (localCart.length > 0){
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.style.visibility = 'visible';
+        shoppingCart.innerHTML = '';
+        localCart.forEach(item => {
+            let search = data.gameData.find((dataItem)=> dataItem.id === item.id) || {};
+            shoppingCart.append(generateCartItem(search));
+        })
+    } else {
+        shoppingCart.innerHTML = '';
+        const noItem = document.querySelector('.no-item-found');
+        const sidebar = document.querySelector('.sidebar');
+        noItem.style.visibility = 'visible';
+        sidebar.style.visibility = 'hidden';
     }
 }
 
 generateCartItems();
 
-const removeFromCart = (id) => {
-    let cartItems = calculateCartItems();
-    console.log('id is ', id.toString());
-    cartItems = cartItems.filter((item) => item.id !== id.toString());
-    localStorage.setItem('cartData', JSON.stringify(cartItems));
+
+let removeFromCart = (productId) => {
+    console.log('id is', productId);
+    console.log(typeof productId);
+    let search = localCart.find(item => item.id === productId);
+    if (search !== undefined) {
+        localCart = localCart.filter(item => item.id !== productId);
+    }
+    else {
+        return;
+    }
+    localStorage.setItem('cartData', JSON.stringify(localCart));
+    calculate(localCart, 'cartCount');
+    search = null;
+    productId = null;
     generateCartItems();
-    calculate(cartItems, 'cartCount');
+}
+
+let moveToWishlist = (productId) => {
+    let search = localWishlist.find(item => item.id === productId);
+    console.log('adding item to wishlist....')
+    if (search === undefined) {
+        localWishlist.push({id:productId});
+        localCart = localCart.filter(item => item.id !== productId);
+        localStorage.setItem('cartData', JSON.stringify(localCart));
+        localStorage.setItem('wishlistData', JSON.stringify(localWishlist));
+        calculate(localCart, 'cartCount');
+        calculate(localWishlist, 'wishlistCount');
+        generateCartItems();
+    }
 }
 
 let calculate = (localStorageDatabase, selectionCountId) => {
     let selectionCountElement = document.getElementById(selectionCountId);
     console.log(localStorageDatabase.length);
     selectionCountElement.innerHTML = localStorageDatabase.length;
+    generateCartItems();
 }
 
-
-calculate(calculateCartItems(), 'cartCount');
+calculate(localCart, 'cartCount');
+calculate(localWishlist, 'wishlistCount');
 
